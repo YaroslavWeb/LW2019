efa44c6d90c139866a841595ee12502d88987d3e
YaroslavWeb <retro.bot220@gmail.ru>
2019-09-07 13:22:22 +0800

    edit 12 var

--- Polkan/Lab2/12var.js
+++ Polkan/Lab2/12var.js
@@ -2,7 +2,11 @@
 let y_razrez,
     s_left,
     s_right,
-    x, y1, y2;
+    x, y1, y2,
+    alpha1, alpha2,
+    seg1, seg2,
+    st1, st2,
+    s1, s2;
 
 let numberMask = IMask(
     document.getElementById('input-mask'), {
@@ -13,17 +17,14 @@ let numberMask = IMask(
         scale: 10
     });
 
-$('.btn').click(() => {
+$('#calc').click(() => {
     x = +$('.form-control').val();
-    alert(x);
     x > 1 || x < -1 ?
-        $('.output').empty().append(`
-    Вы ввели неправильное число
-    `) :
-        mathRazrez();
+        $('.output').empty().append(`Вы ввели неправильное число`) :
+        calculate();
 });
 
-let mathRazrez = () => {
+let calculate = () => {
     if (x > 0 && x <= 1) {
         y1 = Math.sqrt(1 - x ** 2);
         y2 = Math.sqrt(0.25 - (0.5 - x) * (0.5 - x));
@@ -39,104 +40,24 @@ let mathRazrez = () => {
         y2 = 0;
         y_razrez = 1;
     };
+
+    alpha1 = Math.atan(y1 / x);
+    seg1 = 0.5 * (alpha1 - Math.sin(alpha1));
+    st1 = (y1 * (1.0 - x)) / 2.0;
+    s1 = seg1 + st1;
+    alpha2 = Math.atan(y2 / x);
+    seg2 = 0.5 * (alpha2 - Math.sin(alpha2));
+    st2 = (y1 * (1 - x)) / 2;
+    s2 = seg2 + st2;
+    s_right = s1 + s2;
+    s_left = ((Math.PI) / 2) + (Math.PI * 0.25) - s_right;
+
     $('.output').empty().append(`
-        x = ${x}
-        y1 = ${y1.toFixed(4)}
-        y2= ${y2.toFixed(4)}
-        y_Разрез = ${y_razrez.toFixed(4)}
+        x = ${x}|
+        y1 = ${y1.toFixed(4)}|
+        y2= ${y2.toFixed(4)}|
+        y_разрез = ${y_razrez.toFixed(4)}|
+        s_левая = ${s_left.toFixed(4)}|
+        s_правая = ${s_right.toFixed(4)}
     `);
 }
-
-
-
-// alfa:=arctan(y/x);
-
-// segment:=0.5(alfa-sin(alfa));
-
-// St:=(y(1.0-x))/2.0;
-
-// s1:=segment+St;
-
-// alfa1:=arctan(y1/x);
-
-// segment1:=0.5(alfa1-sin(alfa1));
-
-// st1:=(y1(1-x))/2;
-
-// s2:=segment1+st1;
-
-// s_right:=s1+s2;
-
-// s_left:=(pi)+(pi0.25)-S_right;
-
-// end;
-
-// if (x>-1)and(x<0)then
-
-// begin
-
-// y:=sqrt(1.0-(abs(x) abs(x)));
-
-// y1:=sqrt(0.25-(0.5-abs(x))(0.5+abs(x)));
-
-// y_razrez:=y+y1;
-
-// alfa:=arctan(y/abs(x));
-
-// segment:=0.5(alfa-sin(alfa));
-
-// St:=(y(1.0-abs(x)))/2.0;
-
-// s1:=segment+St;
-
-// alfa1:=arctan(y1/abs(x));
-
-// segment1:=0.5(alfa1-sin(alfa1));
-
-// st1:=(y1(1-abs(x)))/2;
-
-// s2:=segment1+st1;
-
-// s_left:=s1+s2;
-
-// s_right:=(pi)+(pi0.25)-S_left;
-
-// end;
-
-// if(x=1)then
-
-// begin
-
-// s_left:=(pi)+(pi0.25);
-
-// s_right:=0;
-
-// end;
-
-// if (x=-1)then
-
-// begin
-
-// s_right:=(pi)+(pi0.25);
-
-// s_left:=0;
-
-// end;
-
-// if (x=0) then
-
-// begin
-
-// s_left:=(((pi)+(pi0.25))/2);
-
-// s_right:=(((pi)+(pi*0.25))/2);
-
-// end;
-
-// WriteLn('y_razrez=',y_razrez:6:4);
-
-// WriteLn('s_left=', s_left:6:4);
-
-// WriteLn('s_right=', s_right:6:4);
-
-// end.
