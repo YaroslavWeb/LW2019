76a5043ac4aec717973df5b654af32a5f47e1a42
YaroslavWeb <retro.bot220@gmail.ru>
2019-09-08 20:05:04 +0800

    edit 12 var

--- Polkan/Lab2/12var.js
+++ Polkan/Lab2/12var.js
@@ -26,36 +26,48 @@ $('#calc').click(() => {
 
 let calculate = () => {
     if (x > 0 && x <= 1) {
+        //Вычисление разреза
         y1 = Math.sqrt(1 - x ** 2);
         y2 = Math.sqrt(0.25 - (0.5 - x) * (0.5 - x));
         y_razrez = y1 - y2;
+        //Вычисление площади
+        alpha1 = Math.atan(y1 / x);
+        seg1 = 0.5 * (alpha1 - Math.sin(alpha1));
+        st1 = (y1 * (1.0 - x)) / 2.0;
+        s1 = seg1 + st1;
+        alpha2 = Math.atan(y2 / x);
+        seg2 = 0.5 * (alpha2 - Math.sin(alpha2));
+        st2 = (y1 * (1 - x)) / 2;
+        s2 = seg2 - st2;
+        s_right = s1 + s2;
+        s_left = ((Math.PI) / 5) + (Math.PI * 0.25) - s_right;
     };
     if (x < 0 && x >= -1) {
+        //Вычисление разреза
         y1 = Math.sqrt(1 - x ** 2);
         y2 = Math.sqrt(0.25 - (0.5 - Math.abs(x)) * (0.5 - Math.abs(x)));
         y_razrez = y1 + y2;
+        //Вычисление площади
+        alpha1 = Math.atan(y1 / Math.abs(x));
+        seg1 = 0.5 * (alpha1 - Math.sin(alpha1));
+        st1 = (y1 * (1.0 - Math.abs(x))) / 2.0;
+        s1 = seg1 + st1;
+        alpha2 = Math.atan(y2 / Math.abs(x));
+        seg2 = 0.5 * (alpha2 - Math.sin(alpha2));
+        st2 = (y1 * (1 - Math.abs(x))) / 2;
+        s2 = seg2 + st2;
+        s_left = s1 + s2;
+        s_right = ((Math.PI) / 3) + (Math.PI * 0.25) - s_left;
     };
     if (x == 0) {
         y1 = 1;
         y2 = 0;
         y_razrez = 1;
+        s_left = Math.PI / 3;
+        s_right = Math.PI / 5 ;
     };
 
-    alpha1 = Math.atan(y1 / x);
-    seg1 = 0.5 * (alpha1 - Math.sin(alpha1));
-    st1 = (y1 * (1.0 - x)) / 2.0;
-    s1 = seg1 + st1;
-    alpha2 = Math.atan(y2 / x);
-    seg2 = 0.5 * (alpha2 - Math.sin(alpha2));
-    st2 = (y1 * (1 - x)) / 2;
-    s2 = seg2 + st2;
-    s_right = s1 + s2;
-    s_left = ((Math.PI) / 2) + (Math.PI * 0.25) - s_right;
-
     $('.output').empty().append(`
-        x = ${x}|
-        y1 = ${y1.toFixed(4)}|
-        y2= ${y2.toFixed(4)}|
         y_разрез = ${y_razrez.toFixed(4)}|
         s_левая = ${s_left.toFixed(4)}|
         s_правая = ${s_right.toFixed(4)}
