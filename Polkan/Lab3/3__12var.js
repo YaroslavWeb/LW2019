1b8f46ff76a6c25881d44ec7a72d0a8709cea50f
YaroslavWeb <retro.bot220@gmail.ru>
2019-09-09 14:46:46 +0800

    Done 3lab polkan

--- Polkan/Lab3/3__12var.js
+++ Polkan/Lab3/3__12var.js
@@ -1,75 +1,69 @@
 "use strict";
-let y_razrez,
-    s_left,
-    s_right,
-    x, y1, y2,
-    alpha1, alpha2,
-    seg1, seg2,
-    st1, st2,
-    s1, s2;
+let x, x_begin, x_end,
+    step, eps, f, t, n, s;
 
-let numberMask = IMask(
-    document.getElementById('input-mask'), {
-        mask: Number,
-        min: -1,
-        max: 1,
-        radix: '.',
-        scale: 10
-    });
+
+IMask(document.getElementById('input-begin'), {
+    mask: Number,
+    radix: '.',
+});
+IMask(document.getElementById('input-end'), {
+    mask: Number,
+    max:100,
+    radix: '.',
+});
+IMask(document.getElementById('input-step'), {
+    mask: Number,
+    radix: '.',
+});
+
+IMask(document.getElementById('input-eps'), {
+    mask: Number,
+    min: 0,
+    max: 0.1,
+    radix: '.',
+});
+
 
 $('#calc').click(() => {
-    x = +$('.form-control').val();
-    x > 1 || x < -1 ?
-        $('.output').empty().append(`Вы ввели неправильное число`) :
-        calculate();
+    $('tbody').empty();
+    x_begin = +$('#input-begin').val();
+    x_end = +$('#input-end').val();
+    step = +$('#input-step').val();
+    eps = +$('#input-eps').val();
+    x = x_begin;
+
+    step != 0 && x_begin <= x_end && step <= x_end ?
+     calculate() :
+    $('tbody').empty().append(`
+    <tr>
+        <td>Введены некоректные данные</td>
+    </tr>
+    `);
 });
 
 let calculate = () => {
-    if (x > 0 && x <= 1) {
-        //Вычисление разреза
-        y1 = Math.sqrt(1 - x ** 2);
-        y2 = Math.sqrt(0.25 - (0.5 - x) * (0.5 - x));
-        y_razrez = y1 - y2;
-        //Вычисление площади
-        alpha1 = Math.atan(y1 / x);
-        seg1 = 0.5 * (alpha1 - Math.sin(alpha1));
-        st1 = (y1 * (1.0 - x)) / 2.0;
-        s1 = seg1 + st1;
-        alpha2 = Math.atan(y2 / x);
-        seg2 = 0.5 * (alpha2 - Math.sin(alpha2));
-        st2 = (y1 * (1 - x)) / 2;
-        s2 = seg2 - st2;
-        s_right = s1 + s2;
-        s_left = ((Math.PI) / 2) - s_right;
-    };
-    if (x < 0 && x >= -1) {
-        //Вычисление разреза
-        y1 = Math.sqrt(1 - x ** 2);
-        y2 = Math.sqrt(0.25 - (0.5 - Math.abs(x)) * (0.5 - Math.abs(x)));
-        y_razrez = y1 + y2;
-        //Вычисление площади
-        alpha1 = Math.atan(y1 / Math.abs(x));
-        seg1 = 0.5 * (alpha1 - Math.sin(alpha1));
-        st1 = (y1 * (1.0 - Math.abs(x))) / 2.0;
-        s1 = seg1 + st1;
-        alpha2 = Math.atan(y2 / Math.abs(x));
-        seg2 = 0.5 * (alpha2 - Math.sin(alpha2));
-        st2 = (y1 * (1 - Math.abs(x))) / 2;
-        s2 = seg2 + st2;
-        s_left = s1 + s2;
-        s_right = (Math.PI / 2) - s_left;
-    };
-    if (x == 0) {
-        y1 = 1;
-        y2 = 0;
-        y_razrez = 1;
-        s_right = (Math.PI / 4) - (Math.PI*0.25)/2;
-        s_left = (Math.PI / 4) + (Math.PI*0.25)/2;;
-    };
+    do {
+        n = 1;
+        t = 1;
+        s = 1;
+        f = ((Math.E ** x) + (Math.E ** -x)) / 2;
 
-    $('.output').empty().append(`
-        y_разрез = ${y_razrez.toFixed(4)}|
-        s_левая = ${s_left.toFixed(4)}|
-        s_правая = ${s_right.toFixed(4)}
-    `);
+        while (Math.abs(t) > eps) {
+            t = (t * x * x) / (2 * n * (2 * n - 1));
+            s = s + t;
+            n = n + 1;
+        }
+        addRow();
+        x = x + step;
+    } while (x < x_end)
 }
+
+let addRow = () => $('tbody').append(`
+    <tr>
+        <td>${x.toFixed(4)}</td>
+        <td>${n}</td>
+        <td>${s.toFixed(8)}</td>
+        <td>${f.toFixed(8)}</td>
+    </tr>
+    `);
