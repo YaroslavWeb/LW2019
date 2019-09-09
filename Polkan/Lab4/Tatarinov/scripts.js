3d0e732329a79d668e97a629867391d0f67519f0
YaroslavWeb <retro.bot220@gmail.ru>
2019-09-09 22:29:40 +0800

    start 4lab Polkan

---
+++ Polkan/Lab4/Tatarinov/scripts.js
@@ -0,0 +1,57 @@
+"use strict";
+let t, s, f, i, n;
+
+
+IMask(document.getElementById('input-n'), {
+    mask: Number,
+    min:0,
+    max:999999,
+    radix: '.',   
+    scale:6 
+});
+
+$('#calc').click(() => {
+    $('tbody').empty();
+    x_begin = +$('#input-begin').val();
+    x_end = +$('#input-end').val();
+    step = +$('#input-n').val();
+    x = x_begin;
+
+    step != 0 && x_begin <= x_end ?
+     calculate() :
+    $('tbody').empty().append(`
+    <tr>
+        <td>Введены некоректные данные</td>
+    </tr>
+    `);
+});
+
+let calculate = () => {
+    do {
+        n = 1;
+        t = 1;
+        s = 1;
+        f = ((Math.E ** x) + (Math.E ** -x)) / 2;
+
+        while (Math.abs(t) > eps) {
+            t = (t * x * x) / (2 * n * (2 * n - 1));
+            s = s + t;
+            n = n + 1;
+        }
+        addRow();
+        x = x + step;
+    } while (x < x_end)
+}
+
+let addRow = () => $('tbody').append(`
+    <tr>
+        <td>${x.toFixed(4)}</td>
+        <td>${n}</td>
+        <td>${s.toFixed(8)}</td>
+        <td>${f.toFixed(8)}</td>
+    </tr>
+    <ul class="list-group list-group-horizontal-md">
+    <li class="list-group-item col-6 text-center">i</li>
+    <li class="list-group-item col-6 text-center">s</li>
+</ul>
+    `);
