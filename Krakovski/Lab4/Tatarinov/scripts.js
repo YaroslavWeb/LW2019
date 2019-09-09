009c549934d4c45f03a79a8e5d7e984ce3d32d99
YaroslavWeb <retro.bot220@gmail.ru>
2019-09-09 21:08:19 +0800

    complete 4 lab Krakov

--- Krakovski/Lab4/Tatarinov/scripts.js
+++ Krakovski/Lab4/Tatarinov/scripts.js
@@ -1,15 +1,26 @@
-let x0 = Math.round(Math.random() * (100 - 1) + 1),
+let x0 = Math.round(Math.random() * (1000 - 1) + 1), 
     a = 630360016,
     m = 2147483647,
     A = [],
     x = x0;
     n = 3000;
-    console.log(x0);
-while(n > 0) {
+
+while (n > 0) {
     if (n == 100) x = a * x0 % m;
     else x = a * x % m;
     A.push(x);
     n--;
-    console.log(n);
 }
-console.log(A);
+
+let i = Math.round((Math.random()*n));
+let k = A[i];
+let xs = A.reduce((sum, curent)=> (sum+curent)/m);
+
+$('.result-items').append(`
+<li class="list-group-item list-group-item-dark">i  = ${i}</li>
+<li class="list-group-item list-group-item-dark">k  = ${k}</li>
+<li class="list-group-item list-group-item-dark">Xs = ${xs.toFixed(2)}</li>
+`)
+
+
+
